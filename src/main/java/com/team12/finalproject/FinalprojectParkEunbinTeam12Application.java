3dfd8fb734f890fec6030543023e99fbb1efa551
박은빈 <joypeb@naver.com>
2022-12-20 09:35:32 +0900

    first commit

---
+++ src/main/java/com/team12/finalproject/FinalprojectParkEunbinTeam12Application.java
@@ -0,0 +1,13 @@
+package com.team12.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectParkEunbinTeam12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectParkEunbinTeam12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/team12/finalproject/FinalprojectParkEunbinTeam12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team12.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectParkEunbinTeam12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
