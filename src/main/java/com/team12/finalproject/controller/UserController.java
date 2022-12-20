fda2d1f6a74d732a8d40c7fb24d1bbb2cd436e46
박은빈 <joypeb@naver.com>
2022-12-20 12:34:48 +0900

    swagger test

--- src/main/java/com/team12/finalproject/controller/UserController.java
+++ src/main/java/com/team12/finalproject/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.team12.finalproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "swagger test hello";
+    }
 
 }
