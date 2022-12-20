df25015b04aade98642193c658eae77435dacad5
박은빈 <joypeb@gmail.com>
2022-12-20 08:05:24 +0000

    Update HelloController.java

--- src/main/java/com/team12/finalproject/controller/HelloController.java
+++ src/main/java/com/team12/finalproject/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.team12.finalproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1")
