77b6fbc48ba3c6826370f6c084a1ab4487b8ae99
박은빈 <joypeb@naver.com>
2022-12-21 23:29:00 +0900

    페이징 테스트

--- src/test/java/com/team12/finalproject/controller/PostControllerTest.java
+++ src/test/java/com/team12/finalproject/controller/PostControllerTest.java
@@ -2,15 +2,20 @@ package com.team12.finalproject.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team12.finalproject.configuration.JwtTokenFilter;
+import com.team12.finalproject.domain.Post;
 import com.team12.finalproject.domain.dto.Response;
 import com.team12.finalproject.domain.dto.post.PostRequest;
 import com.team12.finalproject.domain.dto.post.PostResult;
+import com.team12.finalproject.repository.PostRepository;
 import com.team12.finalproject.service.PostService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.PermissionDeniedDataAccessException;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,8 +28,15 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.web.client.HttpClientErrorException;;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
@@ -68,5 +80,24 @@ class PostControllerTest {
                 .content(objectMapper.writeValueAsBytes(new PostRequest("",""))))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("포스트 리스트 성공")
+    void post_list_s() {
+        //페이징 설정
+        Pageable pageRequest = PageRequest.of(0,20, Sort.by("registeredAt").descending());
+        List<Post> postList = new ArrayList<>();
+        //포스트 추가
+        postList.add(new Post(0,"user1","title1","body1", LocalDateTime.of(2022,11,21,19,20),null,null));
+        postList.add(new Post(1,"user2","title2","body2", LocalDateTime.of(2022,12,21,19,20),null,null));
+        //페이지 처리
+        PageImpl<Post> result = new PageImpl<>(postList, pageRequest, 2L);
+
+        List<Post> postList1 = result.toList();
+
+        for(Post x : postList1) {
+            System.out.println(x.getRegisteredAt());
+        }
     }
 }
