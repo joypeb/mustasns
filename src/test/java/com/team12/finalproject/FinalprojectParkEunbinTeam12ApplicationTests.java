d7b6b2513a6add06ebd2d16a9994768adc7fc391
박은빈 <joypeb@naver.com>
2022-12-22 13:18:22 +0900

    post list test 진행중

--- src/test/java/com/team12/finalproject/FinalprojectParkEunbinTeam12ApplicationTests.java
+++ src/test/java/com/team12/finalproject/FinalprojectParkEunbinTeam12ApplicationTests.java
@@ -2,8 +2,10 @@ package com.team12.finalproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class FinalprojectParkEunbinTeam12ApplicationTests {
 
     @Test
