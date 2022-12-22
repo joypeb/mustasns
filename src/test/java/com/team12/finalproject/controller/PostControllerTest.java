94bca9db70fec781c45895278468e5e8a016ff43
박은빈 <joypeb@naver.com>
2022-12-22 19:50:35 +0900

    Posts 수정 테스트

--- src/test/java/com/team12/finalproject/controller/PostControllerTest.java
+++ src/test/java/com/team12/finalproject/controller/PostControllerTest.java
@@ -8,6 +8,8 @@ import com.team12.finalproject.domain.dto.Response;
 import com.team12.finalproject.domain.dto.post.PostDetailResponse;
 import com.team12.finalproject.domain.dto.post.PostRequest;
 import com.team12.finalproject.domain.dto.post.PostResult;
+import com.team12.finalproject.exception.AppException;
+import com.team12.finalproject.exception.ErrorCode;
 import com.team12.finalproject.repository.PostRepository;
 import com.team12.finalproject.service.PostService;
 import org.junit.jupiter.api.Assertions;
@@ -21,6 +23,7 @@ import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,24 +32,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.web.client.HttpClientErrorException;;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.MockMvc.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @WebMvcTest(PostController.class)
 @ExtendWith(SpringExtension.class)
@@ -108,14 +106,7 @@ class PostControllerTest {
         mockMvc.perform(get("/api/v1/posts/1")
                 .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(
-                        new Response<PostDetailResponse>("",
-                                PostDetailResponse.builder()
-                                        .id(1)
-                                        .title("")
-                                        .body("")
-                                        .userName("")
-                                        .build()))))
+                .content(objectMapper.writeValueAsBytes(1)))
                 .andDo(print())
                 .andExpect(status().isOk());
 
@@ -125,5 +116,69 @@ class PostControllerTest {
         Assertions.assertNotNull(postDetailResult.getResult().getTitle());
         Assertions.assertNotNull(postDetailResult.getResult().getBody());
         Assertions.assertNotNull(postDetailResult.getResult().getUserName());
+    }
+
+    @Test
+    @DisplayName("포스트 수정 성공")
+    @WithMockUser
+    void post_modified_s() throws Exception{
+        when(postService.modifyPost(1,new PostRequest("t","tb"),"")).thenReturn(new Response<>("SUCCESS",new PostResult("",1)));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new PostRequest("t","tb"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("포스트 수정 실패 - 인증 실패")
+    void post_modified_f1() throws Exception{
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostRequest("t","tb"))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("포스트 수정 실패 - 작성자 불일치")
+    @WithMockUser
+    void post_modified_f2() throws Exception{
+        when(postService.modifyPost(1,new PostRequest("",""),"u"))
+                .thenThrow(new AppException(ErrorCode.INVALID_PERMISSION,"userName이 일치하지 않습니다"));
+
+        /*mockMvc.perform(put("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostRequest("t","tb"))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+*/
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/v1/posts/{id}","1")
+                .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsBytes(new PostRequest("","")));
+
+        mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().isUnauthorized());
+    }
+
+
+    @Test
+    @DisplayName("포스트 수정 실패 - 데이터베이스 에러")
+    @WithMockUser
+    void post_modified_f3() throws Exception{
+        when(postService.modifyPost(1,new PostRequest("",""),""))
+                .thenThrow(new AppException(ErrorCode.DATABASE_ERROR,"DB에러"));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostRequest("t","tb")))
+                        .queryParam("id","1"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError());
     }
 }
