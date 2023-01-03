338c9bbb1747e70e28b51934d7c701196b74a35a
박은빈 <joypeb@naver.com>
2023-01-03 17:25:35 +0900

    [feat] comment관련 에러코드 추가

--- src/main/java/com/team12/finalproject/exception/ErrorCode.java
+++ src/main/java/com/team12/finalproject/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
-    INVALID_USERROLE(HttpStatus.UNAUTHORIZED,"userRole이 잘못되었습니다");
+    INVALID_USERROLE(HttpStatus.UNAUTHORIZED,"userRole이 잘못되었습니다"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 comment를 찾을 수 없습니다")
+    ;
 
     HttpStatus httpStatus;
     String message;
