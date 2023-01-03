3df3000229f9421dad14652d6e668479b4207476
박은빈 <joypeb@naver.com>
2023-01-03 11:58:42 +0900

    [fix] admin 변경 response 리팩토링

--- src/main/java/com/team12/finalproject/domain/dto/adminRoleChange/AdminRoleChangeRequest.java
+++ src/main/java/com/team12/finalproject/domain/dto/adminRoleChange/AdminRoleChangeRequest.java
@@ -1,6 +1,5 @@
 package com.team12.finalproject.domain.dto.adminRoleChange;
 
-import com.team12.finalproject.domain.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/com/team12/finalproject/domain/dto/adminRoleChange/AdminRoleChangeResponse.java
+++ src/main/java/com/team12/finalproject/domain/dto/adminRoleChange/AdminRoleChangeResponse.java
@@ -1,5 +1,6 @@
 package com.team12.finalproject.domain.dto.adminRoleChange;
 
+import com.team12.finalproject.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +13,11 @@ import lombok.NoArgsConstructor;
 public class AdminRoleChangeResponse {
     private String message;
     private int userId;
+
+    public static AdminRoleChangeResponse response(User user) {
+        return AdminRoleChangeResponse.builder()
+                .message(String.format("userRole이 %s로 변경되었습니다", String.valueOf(user.getRole())))
+                .userId(user.getId())
+                .build();
+    }
 }
