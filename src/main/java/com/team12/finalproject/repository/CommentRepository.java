7e80d75be64ddb885ed7d3765189a1fd3651fe24
박은빈 <joypeb@naver.com>
2023-01-03 13:19:28 +0900

    [fix] comment레포지토리 생성

---
+++ src/main/java/com/team12/finalproject/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.team12.finalproject.repository;
+
+import com.team12.finalproject.domain.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    Page<Comment> findAllByPostId(int postId, Pageable pageable);
+}
