74987b06e0300646845df412326639e469b68bdc
박은빈 <joypeb@naver.com>
2023-01-05 13:43:10 +0900

    [feat] Alarm엔티티작성

---
+++ src/main/java/com/team12/finalproject/domain/entity/Alarm.java
@@ -0,0 +1,28 @@
+package com.team12.finalproject.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.team12.finalproject.domain.role.AlarmType;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Alarm extends BaseEntity{
+    //fromUserId : 알림을 발생시킨 아이디
+    //targetId : 알림이 발생된 post의 id
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private AlarmType alarmType;
+    private int fromUserId;
+    private int targetId;
+    private String text;
+    private LocalDateTime deleted_at;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    @JsonIgnore
+    private User user;
+}
---
+++ src/main/java/com/team12/finalproject/domain/role/AlarmType.java
@@ -0,0 +1,5 @@
+package com.team12.finalproject.domain.role;
+
+public enum AlarmType {
+    NEW_COMMENT_ON_POST,NEW_LIKE_ON_POST;
+}
