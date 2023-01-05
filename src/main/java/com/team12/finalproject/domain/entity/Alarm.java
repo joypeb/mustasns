55326b8548e1d070a6f169de0e11c2010d7913f6
박은빈 <joypeb@naver.com>
2023-01-05 13:43:55 +0900

    [feat] Alarm엔티티작성

--- src/main/java/com/team12/finalproject/domain/entity/Alarm.java
+++ src/main/java/com/team12/finalproject/domain/entity/Alarm.java
@@ -2,11 +2,21 @@ package com.team12.finalproject.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.team12.finalproject.domain.role.AlarmType;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE alarm SET deleted_at = current_timestamp WHERE id = ?")
+@Where(clause = "deleted_at is NULL")
 public class Alarm extends BaseEntity{
     //fromUserId : 알림을 발생시킨 아이디
     //targetId : 알림이 발생된 post의 id
